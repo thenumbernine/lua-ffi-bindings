a4abebaba35edfa4a6cbd971536ae30fb3a79954
Christopher Moore <christopher.e.moore@gmail.com>
2018-09-13 08:40:51 +0200

    more ffi stuff

--- c/stdio.lua
+++ c/stdio.lua
@@ -10,14 +10,17 @@ enum {
 
 typedef void FILE;
 
-int fclose(FILE *);
+int fclose(FILE *stream);
+int feof(FILE *stream);
+int fflush(FILE *stream);
 FILE *fopen(const char *filename, const char *mode);
 size_t fread(void *ptr, size_t size, size_t nitems, FILE *stream);
 int fseek(FILE *stream, long offset, int whence);
 long ftell(FILE *stream);
 size_t fwrite(const void *ptr, size_t size, size_t nitems, FILE *stream);
-void rewind(FILE *stream);
-int feof(FILE *stream);
+int printf(const char *fmt, ...);
+int sprintf(char *s, const char*fmt, ...);
 int remove(const char *path);
+void rewind(FILE *stream);
 
 ]]
--- c/stdlib.lua
+++ c/stdlib.lua
@@ -1,6 +1,9 @@
 local ffi = require 'ffi'
 
 ffi.cdef[[
+int rand();
+void srand(unsigned int seed);
+
 void *malloc(size_t size);
 void free(void *ptr);
 void *calloc(size_t nmemb, size_t size);
