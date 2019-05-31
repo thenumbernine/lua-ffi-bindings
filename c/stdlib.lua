4b9dcc77d354898cdba87d8801fb882c1d14b126
Christopher Moore <christopher.e.moore@gmail.com>
2019-05-31 19:38:56 +0800

    added some glu stuff

--- c/stdlib.lua
+++ c/stdlib.lua
@@ -8,4 +8,7 @@ void *malloc(size_t size);
 void free(void *ptr);
 void *calloc(size_t nmemb, size_t size);
 void *realloc(void *ptr, size_t size);
+
+typedef int (*comparison_fn_t)(const void*, const void*, void*);
+void qsort(void* base, size_t nmemb, size_t size, comparison_fn_t compar); 
 ]]
