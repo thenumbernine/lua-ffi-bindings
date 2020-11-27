06cdc2c3567208c8a759197876af37152c7dd883
Christopher Moore <christopher.e.moore@gmail.com>
2020-11-27 13:09:38 +0400

    added env functions to stdlib

--- c/stdlib.lua
+++ c/stdlib.lua
@@ -11,4 +11,12 @@ void *realloc(void *ptr, size_t size);
 
 typedef int (*comparison_fn_t)(const void*, const void*, void*);
 void qsort(void* base, size_t nmemb, size_t size, comparison_fn_t compar); 
+
+int setenv(const char* name, const char* value, int overwrite);
+int unsetenv(const char *name);
+char *getenv(const char *name);
+char *secure_getenv(const char *name);
+int clearenv();
+int putenv(char *string);
+extern char **environ;
 ]]
