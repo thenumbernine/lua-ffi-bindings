f105c40891bc567a865acee4e8a7a37022d5c917
Christopher Moore <christopher.e.moore@gmail.com>
2020-11-27 15:31:03 +0400

    added time functions

--- c/unistd.lua
+++ c/unistd.lua
@@ -9,6 +9,14 @@ extern char* getcwd(char* buf, size_t size);
 int _chdir(const char* path);
 extern char* _getcwd(char* buf, size_t size);
 
+struct timespec {
+    time_t tv_sec;        /* seconds */
+    long   tv_nsec;       /* nanoseconds */
+};
+
+unsigned int sleep(unsigned int seconds);
+int usleep(useconds_t usec);
+int nanosleep(const struct timespec *req, struct timespec *rem);
 ]]
 
 -- I can't change ffi.C.getcwd to ffi.C._getcwd in the case of Windows
