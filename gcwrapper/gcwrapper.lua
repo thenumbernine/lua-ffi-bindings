21337177e50346e26b05ef1481f2595ad0c84bf9
thenumbernine <christopher.e.moore@gmail.com>
2022-01-11 17:01:52 +0600

    cleaned up retain() and release(), added my own refcounting.  before, release() was only used for freeing resources, but now it does proper refcounting (in addition to any hypothetical refcounting done by the passed retain and release functions, like in opencl.

--- gcwrapper/gcwrapper.lua
+++ gcwrapper/gcwrapper.lua
@@ -16,6 +16,10 @@ args:
 	retain = (optional) retain function.  if none is provided then this does nothing.
 	release = release function.
 
+NOTICE that the template init expects a valid, retained, id to be passed as a single argument.
+This means there is no initial retain() called, only a final release() upon destruction.
+Additional retain() or release()'s can be manually called by the API.
+
 fields:
 	id = an instance of 'ctype'
 	gc = an instance of our luajit ffi cdata object whose __gc field calls the release() on our .id
@@ -29,54 +33,94 @@ local function GCWrapper(args)
 	local retain = args.retain
 	local gctype = args.gctype or 'autorelease_'..ctype
 
+	local template
+
+
 	ffi.cdef([[
 struct ]]..gctype..[[ {
 	]]..ctype..[[ ptr[1];
+	int refcount;
 };
 typedef struct ]]..gctype..' '..gctype..[[;
 ]])
+	
+	local function performRelease(gc)
+		gc.refcount = gc.refcount - 1
+		local result = release(gc.ptr[0])
+		if gc.refcount <= 0 then
+			if gc.ptr[0] ~= nil then
+				-- clear gc.ptr[0] upon final release so future release()'s don't try to free it twice
+				gc.ptr[0] = nil
+			else
+				if template.debugleaks then
+					print("tried to free object "..tostring(gc).." with refcount 0, but it had already been freed.  did someone else release() it?")
+				end
+			end
+		end
+		return result
+	end
+
 	local gcType = ffi.metatype(gctype, {
 		__gc = function(gc)
-			-- clear gc.ptr[0] upon final release
-			if gc.ptr[0] ~= nil then
-				release(gc.ptr[0])
-				gc.ptr[0] = nil
+			performRelease(gc)	-- throw away result, __gc doesn't want it
+			if gc.refcount > 0 then
+				-- assume someone else retained it?
+				if template.debugleaks then
+					print("leaking object "..tostring(gc).." with refcount "..gc.refcount.." ... did someone else retain() it?")
+				end
+			else
+				if template.debugleaks then
+					print("freeing object "..tostring(gc).." with refcount "..gc.refcount)
+				end
 			end
 		end,
 	})
 
-	local template = class()
+	template = class()
 	template.gctype = gctype
+
+	-- [[ enable this for debugging
+	template.debugleaks = true
+	--]]
 
 	-- TODO only use gc.ptr[0] instead of id?
 	function template:init(id)
-		assert(id ~= nil, "expected id")
+		--[[
+		TODO I can't always expect 'id' to be valid, esp for the case of creating 'cl.event' objects that have not yet been initialized, 
+		but will be passed into cl routines for initialization later ...
+		in their case, how does memory management pan out?
+		--]]
+		id = ffi.cast(ctype, id)
+
 		-- release-upon-gc/dtor
 		self.gc = gcType()
 		self.gc.ptr[0] = id
+		self.gc.refcount = 1;
 		self.id = id
 	end
 
-	-- make release() interoperable with delete() of gl's gc
-	-- that means sometimes objects won't have a retain() function ...
-	if retain then
-		function template:retain()
+	-- Make release() interoperable with delete() of gl's gc
+	-- That means sometimes objects won't have a retain() function ...
+	-- I would call this 'performRetain', but it really only operates here (or maybe once upon init when refcount is set to 1).
+	function template:retain()
+		if self.gc.ptr[0] == nil then
+			-- error or warning?
+			error("tried to retain() an object already freed.")
+		end
+		self.gc.refcount = self.gc.refcount + 1
+		if retain then
 			return retain(self.id)
 		end
-	else
-		function template:retain() end
 	end
 
 	function template:release()
-		-- single release() statement matches the cl.hpp code, which itself relied on retain/release calls to refcount
-		--return release(self.id)
-		-- but lua does its own refcounting, so retain just needs to be called once upon creation and release once upon delete
-		if self.gc.ptr[0] ~= nil then
-			local result = release(self.gc.ptr[0])
-			self.gc.ptr[0] = nil
-			self.id = nil
-			return result
-		end
+		--[[ single release() statement matches the cl.hpp code, which itself relied on retain/release calls to refcount
+		return release(self.id)
+		--]]
+		-- [[ but lua does its own refcounting, so retain just needs to be called once upon creation and release once upon delete
+		-- does the decrement and freeing
+		return performRelease(self.gc)
+		--]]
 	end
 
 	return template
