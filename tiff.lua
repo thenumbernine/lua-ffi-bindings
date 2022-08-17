2c449a97b0e0eeff8634eb4273b4ff413dd47de6
Christopher Moore <christopher.e.moore@gmail.com>
2022-08-17 10:15:02 +0600

    more

--- tiff.lua
+++ tiff.lua
@@ -1,7 +1,8 @@
 local ffi = require 'ffi'
 local tiff
 if ffi.os == 'Windows' then
-	require 'ffi.Windows.tiff'
+	--require 'ffi.Windows.tiff'	-- oops, not yet
+	require 'ffi.Linux.tiff'
 	tiff = ffi.load(os.getenv'LUAJIT_LIBPATH' .. [[\bin\Windows\]] .. ffi.arch .. [[\tiff.dll]])
 else
 	require 'ffi.Linux.tiff'
