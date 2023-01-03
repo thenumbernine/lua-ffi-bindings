c200002d37c79ad753fd08b1d085ee022bd17df2
Christopher Moore <christopher.e.moore@gmail.com>
2023-01-03 21:24:55 +0700

    oops that got filtered out

--- Linux/OpenGL.lua
+++ Linux/OpenGL.lua
@@ -2347,7 +2347,7 @@ enum { GL_UNIFORM_BLOCK_ACTIVE_UNIFORM_INDICES = 35395 };
 enum { GL_UNIFORM_BLOCK_REFERENCED_BY_VERTEX_SHADER = 35396 };
 enum { GL_UNIFORM_BLOCK_REFERENCED_BY_GEOMETRY_SHADER = 35397 };
 enum { GL_UNIFORM_BLOCK_REFERENCED_BY_FRAGMENT_SHADER = 35398 };
-/* #define GL_INVALID_INDEX                  0xFFFFFFFFu ### string, not number "0xFFFFFFFFu" */
+enum { GL_INVALID_INDEX = 0xFFFFFFFFu };
 typedef void ( * PFNGLDRAWARRAYSINSTANCEDPROC) (GLenum mode, GLint first, GLsizei count, GLsizei instancecount);
 typedef void ( * PFNGLDRAWELEMENTSINSTANCEDPROC) (GLenum mode, GLsizei count, GLenum type, const void *indices, GLsizei instancecount);
 typedef void ( * PFNGLTEXBUFFERPROC) (GLenum target, GLenum internalformat, GLuint buffer);
