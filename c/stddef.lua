9635160b19339220e3f878cdf3d5239d4e99db21
Christopher Moore <christopher.e.moore@gmail.com>
2023-07-28 11:55:04 +0800

    more

--- c/stddef.lua
+++ c/stddef.lua
@@ -1,5 +1,3 @@
--- TODO any easy way to automatically override files per-os and per-arch?
--- kinda like I'm doing in the ffi.load table ...
 local ffi = require 'ffi'
 if ffi.os == 'Windows' then
 	return require 'ffi.Windows.c.stddef'
