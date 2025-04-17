3f32fe9b97e2bc056833e900d078b63ee8a55379
Christopher Moore <christopher.e.moore@gmail.com>
2025-04-17 16:43:07 +0800

    bleh, more sorting library paths out

--- load.lua
+++ load.lua
@@ -21,60 +21,8 @@ If no value exists then the key will be used as the default name to search for t
 All $names will be resolved as environment variables.
 --]]
 local ffi = require 'ffi'
-return setmetatable({
-	cimgui_sdl = {
-		-- why doesn't this automatically search here?
-		Linux = '/usr/local/lib/libcimgui_sdl.so',
-	},
-	GL = {
-		Windows = 'OpenGL32',
-		OSX = '/System/Library/Frameworks/OpenGL.framework/OpenGL',	-- builtin crap
-		--OSX = '/usr/local/lib/libGL.dylib',	-- osx brew mesa
-	},
-	GLU = {
-		Windows = 'GLU32',
-		OSX = '/System/Library/Frameworks/OpenGL.framework/OpenGL',	-- builtin crap
-		--OSX = '/usr/local/lib/libGLU.dylib',	-- osx brew mesa
-	},
-	
-	-- hmm, GLES library names are funny
-	-- GLES1, GLES/gl.h uses GLESv1_CM.so
-	-- GLES2, GLES2/gl2.h uses GLESv2.so
-	-- GLES3, GLES3/gl3.h uses ... GLESv2.so as well
-	GLESv2 = {
-		OSX = '/System/Library/Frameworks/OpenGL.framework/OpenGL',	-- builtin crap
-		--OSX = '/System/Library/Frameworks/CoreImage.framework/Versions/A/Frameworks/libWrapGLES.dylib',	-- dlopen says incompatible platforms
-		--OSX = '/usr/local/lib/libGLESv2.dylib',	-- osx brew mesa
-	},
 
-	EGL = {
-		OSX = '/usr/local/lib/libEGL.dylib',
-	},
-
-	OpenCL = {
-		OSX = '/System/Library/Frameworks/OpenCL.framework/OpenCL',
-	},
-
-	-- can ffi.load"serial/hdf5" work? hmm...
-	hdf5 = {Linux = '/usr/lib/x86_64-linux-gnu/hdf5/serial/libhdf5.so'},
-	png ={Windows = 'libpng16'},
-	z = {Windows = 'zlib'},
-	jpeg = {
-		-- For Windows msvc turbojpeg 2.0.3 cmake wouldn't build, so i used 2.0.4 instead
-		-- I wonder if this is the reason for the few subtle differences
-		-- TODO rebuild linux with 2.0.4 and see if they go away?
-		Windows = 'jpeg8',
-		-- for Linux, libturbojpeg 2.1.2 (which is not libjpeg-turbo *smh* who named this)
-		-- the header generated matches libturbojpeg 2.0.3 for Ubuntu ... except the version macros
-	},
-	openal = {
-		Windows = 'OpenAL32',
-		OSX = '/System/Library/Frameworks/OpenAL.framework/OpenAL',
-	},
-	vorbis = {Windows = 'libvorbis-0'},
-	vorbisfile = {Windows = 'libvorbisfile-3'},
-	ogg = {Windows = 'libogg-0'},
-}, {
+local ffi_load = setmetatable({}, {
 	__call = function(self, reqname)
 		assert(type(reqname) == 'string', "expected string")
 		local v = self[reqname]
@@ -99,3 +47,84 @@ return setmetatable({
 		return ffi.load(v)
 	end,
 })
+
+-- like op.safeindex but with default-assignments as you go
+local function lookup(...)
+	local t = ffi_load
+	for i=1,select('#', ...) do
+		local k = select(i, ...)
+		if t[k] == nil then t[k] = {} end
+		t = t[k]
+	end
+	return t
+end
+
+
+-- if ever ffi.load() searching for the name will fail, just specify its override location here:
+
+
+-- ================================ Linux ================================
+
+
+-- Why doesn't this automatically search here?  Maybe it does and I just forgot to run ldconfig?
+lookup'cimgui_sdl'.Linux = '/usr/local/lib/libcimgui_sdl.so'
+
+lookup'hdf5'.Linux = '/usr/lib/x86_64-linux-gnu/hdf5/serial/libhdf5.so'
+
+
+-- ================================ Windows ================================
+
+
+lookup'GL'.Windows = 'OpenGL32'
+lookup'GLU'.Windows = 'GLU32'
+
+-- can ffi.load"serial/hdf5" work? hmm...
+lookup'png'.Windows = 'libpng16'
+lookup'z'.Windows = 'zlib'
+
+-- For Windows msvc turbojpeg 2.0.3 cmake wouldn't build, so i used 2.0.4 instead
+-- I wonder if this is the reason for the few subtle differences
+-- TODO rebuild linux with 2.0.4 and see if they go away?
+lookup'jpeg'.Windows = 'jpeg8'
+-- for Linux, libturbojpeg 2.1.2 (which is not libjpeg-turbo *smh* who named this)
+-- the header generated matches libturbojpeg 2.0.3 for Ubuntu ... except the version macros
+
+lookup'openal'.Windows = 'OpenAL32'
+lookup'vorbis'.Windows = 'libvorbis-0'
+lookup'vorbisfile'.Windows = 'libvorbisfile-3'
+lookup'ogg'.Windows = 'libogg-0'
+
+
+-- ================================ OSX ================================
+
+
+lookup'GL'.OSX = '/System/Library/Frameworks/OpenGL.framework/OpenGL'	-- builtin crap
+--lookup'GL'.OSX = '/usr/local/lib/libGL.dylib'							-- osx brew mesa ... which, just having this installed, after the last libSDL2 brew update, caused *every single thing* that used brew SDL2 to segfault upon start.
+
+lookup'GLU'.OSX = '/System/Library/Frameworks/OpenGL.framework/OpenGL'	-- builtin crap
+--lookup'GLU'.OSX = '/usr/local/lib/libGLU.dylib'							-- osx brew mesa ... which, just having this installed, after the last libSDL2 brew update, caused *every single thing* that used brew SDL2 to segfault upon start.
+
+-- hmm, GLES library names are funny
+-- GLES1, GLES/gl.h uses GLESv1_CM.so
+-- GLES2, GLES2/gl2.h uses GLESv2.so
+-- GLES3, GLES3/gl3.h uses ... GLESv2.so as well
+-- (on Linux?)
+lookup'GLESv2'.OSX = '/System/Library/Frameworks/OpenGL.framework/OpenGL'	-- builtin crap
+--lookup'GLESv2'.OSX = '/System/Library/Frameworks/CoreImage.framework/Versions/A/Frameworks/libWrapGLES.dylib'	-- dlopen says incompatible platforms
+--lookup'GLESv2'.OSX = '/usr/local/lib/libGLESv2.dylib'	-- osx brew mesa
+
+lookup'EGL'.OSX = '/usr/local/lib/libEGL.dylib'
+
+lookup'OpenCL'.OSX = '/System/Library/Frameworks/OpenCL.framework/OpenCL'
+lookup'openal'.OSX = '/System/Library/Frameworks/OpenAL.framework/OpenAL'
+
+-- Welp, OSX luajit stopped linking against /usr/local/lib by default ...
+-- And setting DYLD_LIBRARY_PATH crashes library conflicts (which will probably start happening anyways as I change this more)
+-- But here I go trying to point to /usr/local/lib on OSX for all the libs I reference in here.
+-- Another reason to use malkia's model and just package all the binaries in the repo itself.
+lookup'SDL2'.OSX = '/usr/local/lib/libSDL2.dylib'
+lookup'png'.OSX = '/usr/local/lib/libpng.dylib'
+lookup'clip'.OSX = '/usr/local/lib/libclip.dylib'
+
+
+return ffi_load
