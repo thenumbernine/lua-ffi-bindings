9bb7a380fbedd205f8ec34622205685f1ea0c638
Christopher Moore <christopher.e.moore@gmail.com>
2026-01-26 11:46:32 +0800

    more

---
+++ Windows/c/semaphore.lua
@@ -0,0 +1,28 @@
+-- copied from Linux
+local ffi = require 'ffi'
+require 'ffi.req' 'c.sys.types'
+require 'ffi.req' 'c.pthread'
+ffi.cdef[[
+typedef union
+{
+  char __size[32];
+  long int __align;
+} sem_t;
+extern int sem_init (sem_t *__sem, int __pshared, unsigned int __value)
+  __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__nonnull__ (1)));
+extern int sem_destroy (sem_t *__sem) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__nonnull__ (1)));
+extern sem_t *sem_open (const char *__name, int __oflag, ...)
+  __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__nonnull__ (1)));
+extern int sem_close (sem_t *__sem) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__nonnull__ (1)));
+extern int sem_unlink (const char *__name) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__nonnull__ (1)));
+extern int sem_wait (sem_t *__sem) __attribute__ ((__nonnull__ (1)));
+extern int sem_timedwait (sem_t *__restrict __sem,
+     const struct timespec *__restrict __abstime)
+  __attribute__ ((__nonnull__ (1, 2)));
+extern int sem_trywait (sem_t *__sem) __attribute__ ((__nothrow__)) __attribute__ ((__nonnull__ (1)));
+extern int sem_post (sem_t *__sem) __attribute__ ((__nothrow__)) __attribute__ ((__nonnull__ (1)));
+extern int sem_getvalue (sem_t *__restrict __sem, int *__restrict __sval)
+  __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__nonnull__ (1, 2)));
+]]
+
+return ffi.C
--- Windows/c/unistd.lua
+++ Windows/c/unistd.lua
@@ -1,12 +1,49 @@
 local ffi = require 'ffi'
 require 'ffi.Windows.c.direct'  -- get our windows defs
 local lib = ffi.C
--- TODO I see the orig name prototypes in direct.h ...
--- ... so do I even need the Lua alias anymore?
-return setmetatable({
+
+local M = {
+	_SC_NPROCESSORS_ONLN = 1,
+
 	chdir = lib._chdir,
 	getcwd = lib._getcwd,
 	rmdir = lib._rmdir,
-}, {
+}
+
+-- https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getsysteminfo 
+ffi.cdef[[
+typedef struct _SYSTEM_INFO {
+  union {
+    uint32_t dwOemId;
+    struct {
+      uint16_t wProcessorArchitecture;
+      uint16_t wReserved;
+    } DUMMYSTRUCTNAME;
+  } DUMMYUNIONNAME;
+  uint32_t     dwPageSize;
+  void*    lpMinimumApplicationAddress;
+  void*    lpMaximumApplicationAddress;
+  uint32_t* dwActiveProcessorMask;
+  uint32_t     dwNumberOfProcessors;
+  uint32_t     dwProcessorType;
+  uint32_t     dwAllocationGranularity;
+  uint16_t      wProcessorLevel;
+  uint16_t      wProcessorRevision;
+} SYSTEM_INFO;
+void GetSystemInfo(SYSTEM_INFO *lpSystemInfo);
+]]
+
+-- is there no Windows POSIX implementation of sysconf?
+function M.sysconf(i)
+	if i == M._SC_NPROCESSORS_ONLN then
+		local sys_info = ffi.new'SYSTEM_INFO'
+		ffi.C.GetSystemInfo(sys_info)
+		return sys_info.dwNumberOfProcessors
+	end
+end
+
+-- TODO I see the orig name prototypes in direct.h ...
+-- ... so do I even need the Lua alias anymore?
+return setmetatable(M, {
 	__index = lib,
 })
